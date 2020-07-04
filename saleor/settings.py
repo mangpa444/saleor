f5d1655076fdaca7a7b875a524f5437bc339c6a2
mangpa444 <51093730+mangpa444@users.noreply.github.com>
2020-07-04 00:56:10 -0500

    Update settings.py

--- saleor/settings.py
+++ saleor/settings.py
@@ -64,7 +64,7 @@ INTERNAL_IPS = get_list(os.environ.get("INTERNAL_IPS", "127.0.0.1"))
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgres://saleor:saleor@localhost:5432/saleor", conn_max_age=600
+        default="postgres://mtengjwo:w6IClB5aueMs2K-dPV3xGs7xROeCtkfi@john.db.elephantsql.com:5432/mtengjwo", conn_max_age=600
     )
 }
 
@@ -198,7 +198,7 @@ TEMPLATES = [
 ]
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("aRSyZAsWwlE0ueRHoJkZxH57")
 
 if not SECRET_KEY and DEBUG:
     warnings.warn("SECRET_KEY not configured, using a random temporary key.")
